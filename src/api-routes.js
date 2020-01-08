6415a262673993e04e8a0344cfcd6a8bc895b425
Yevhen Tataryn <tatarin.e.m@gmail.com>
2020-01-09 00:38:54 +0100

    added base request to API

---
+++ src/api-routes.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://api.exchangeratesapi.io';
+export const LATEST = '/latest';
---
+++ src/service.js
@@ -0,0 +1,13 @@
+import { API_URL, LATEST } from './api-routes';
+
+// https://api.exchangeratesapi.io/latest?base=EUR
+
+export function fetchRates(base) {
+  return fetch(`${API_URL}${LATEST}?base=${base}`)
+    .then(res => res.json());
+    // TODO: handle errors
+}
+
+
+
+
